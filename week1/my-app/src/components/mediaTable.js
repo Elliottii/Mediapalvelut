c81992de69730c6cfce6bd79c00212fe123d0f95
Eeli Klemettilä <eeli.klemettila@gmail.com>
2020-04-21 16:48:54 +0300

    week2-a

--- week1/my-app/src/components/mediaTable.js
+++ week1/my-app/src/components/mediaTable.js
@@ -1,24 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import MediaRow from './mediaRow';
-const baseUrl = 'http://media.mw.metropolia.fi/wbma/';
+import {useAllMedia} from '../hooks/ApiHooks';
 
 const MediaTable = () => {
-  const [picArray, setPicArray] = useState([]);
-  const loadMedia = async () => {
-    const response = await fetch(baseUrl + 'media');
-    const json = await response.json();
-    const items = await Promise.all(json.map(async (item) => {
-      const response = await fetch(baseUrl + 'media/' + item.file_id);
-      return await response.json();
-    }));
-    console.log(items);
-    setPicArray(items);
-  };
+  const picArray = useAllMedia();
 
-  useEffect(() => {
-    loadMedia();
-  }, []);
-
+  console.log(picArray);
   return (
     <table>
       <tbody>
